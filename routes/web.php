36d1b430c2b6a9925a28600d0947ed789e28ccb2
Rizwan Miraj <miraj801@gmail.com>
2024-04-07 23:45:50 +0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductsController::class, 'index']);
+Route::post('/save', [ProductsController::class, 'save'])->name('save');
